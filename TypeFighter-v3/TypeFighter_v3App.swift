fb673bc79bd9b0297577433fd5071c330b8272b3
AkselLofstedtNielsen <aksel98@outlook.com>
2025-03-24 15:06:27 +0100

    Initial Commit

---
+++ TypeFighter-v3/TypeFighter_v3App.swift
@@ -0,0 +1,17 @@
+//
+//  TypeFighter_v3App.swift
+//  TypeFighter-v3
+//
+//  Created by Aksel Nielsen on 2025-03-24.
+//
+
+import SwiftUI
+
+@main
+struct TypeFighter_v3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TypeFighter-v3Tests/TypeFighter_v3Tests.swift
@@ -0,0 +1,17 @@
+//
+//  TypeFighter_v3Tests.swift
+//  TypeFighter-v3Tests
+//
+//  Created by Aksel Nielsen on 2025-03-24.
+//
+
+import Testing
+@testable import TypeFighter_v3
+
+struct TypeFighter_v3Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
